453500df3206f67738429ec7cf7141fc65e77665
Pablo Méndez <pablomendez778@gmail.com>
2020-09-24 20:44:11 -0600

    Escala de tiempo lista

---
+++ Assets/Scripts/SmallMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SmallMenu : MonoBehaviour
+{
+
+    public Slider slider;
+    public Text textbox;
+
+    void Start()
+    {
+        changeTimeScale(-10f);
+    }
+
+    private void changeTimeScale(float potencia)
+    {
+        Time.timeScale = Mathf.Pow(10, potencia);
+        textbox.text = "Escala de Tiempo: " + Time.timeScale;
+    }
+    
+    public void changeTimeScaleSlider()
+    {
+        changeTimeScale(slider.value);
+    }
+}
