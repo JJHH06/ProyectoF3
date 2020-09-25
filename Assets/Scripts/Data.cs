3037403a31d804118c09a05155d79e99bb95a968
Pablo Méndez <pablomendez778@gmail.com>
2020-09-24 18:42:28 -0600

    Pues ya casi, solo que tengo algunos errores misteriosos

---
+++ Assets/Scripts/Data.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Data : MonoBehaviour
+{
+    public static Data datos;
+    public double velocidad;
+    public double angulo_velocidad;
+    public double magnitud_campo;
+    public int n_particula;
+
+
+    void Awake()
+    {
+        if (Data.datos == null)
+            Data.datos = this;
+        else if (Data.datos != this)
+            Destroy(this.gameObject);
+
+        DontDestroyOnLoad(this);
+    }
+
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    [Header("Para la Navegación")]
+    public GameObject inicio;
+    public GameObject autores;
+    public GameObject preparacion;
+
+    [Space(10)]
+    [Header("Para la lectura de datos")]
+    public InputField velocidad;
+    public InputField angulo_velocidad;
+    public InputField magnitud_campo;
+    public InputField n_particula;
+
+    public void toInicio()
+    {
+        inicio.SetActive(true);
+        autores.SetActive(false);
+        preparacion.SetActive(false);
+    }
+
+    public void toAutores()
+    {
+        inicio.SetActive(false);
+        autores.SetActive(true);
+        preparacion.SetActive(false);
+    }
+
+    public void toPreparacion()
+    {
+        inicio.SetActive(false);
+        autores.SetActive(false);
+        preparacion.SetActive(true);
+    }
+
+    public void trySimulation()
+    {
+        Debug.Log("Verificando la información...");
+        
+        try
+        { 
+            //Data.datos.velocidad = double.Parse(velocidad.text);
+            //Data.datos.angulo_velocidad = double.Parse(angulo_velocidad.text);
+            //Data.datos.magnitud_campo = double.Parse(magnitud_campo.text);
+            //Data.datos.n_particula = int.Parse(n_particula.text);
+
+            toSimulacion();
+        }
+        catch
+        {
+            Debug.Log("Ahí algún error en los inputs.");
+        }
+    }
+
+    private void toSimulacion()
+    {
+        Debug.Log("Se abrió la simulacion.");
+        /*
+        Debug.Log("Velocidad: " + Data.datos.velocidad);
+        Debug.Log("Angulo: " + Data.datos.angulo_velocidad);
+        Debug.Log("Campo: " + Data.datos.magnitud_campo);
+        Debug.Log("Partícula: " + Data.datos.n_particula);
+        */
+    }
+
+    public void exitGame()
+    {
+        
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+        
+    }
+}
